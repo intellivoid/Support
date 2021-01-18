1e0617be98c69bc76152e5295af29c40e218c444
Netkas <netkas@intellivoid.info>
2021-01-18 11:30:35 -0500

    Updated for PPM 1.1.2.0

--- src/Support/Exceptions/InvalidSourceException.php
+++ src/Support/Exceptions/InvalidSourceException.php
@@ -3,19 +3,20 @@
 
     namespace Support\Exceptions;
 
+    use Exception;
     use Support\Abstracts\ExceptionCodes;
 
     /**
      * Class InvalidSourceException
      * @package Support\Exceptions
      */
-    class InvalidSourceException extends \Exception
+    class InvalidSourceException extends Exception
     {
         /**
          * InvalidSourceException constructor.
          */
         public function __construct()
         {
-            parent::__construct('The source is invalid', ExceptionCodes::InvalidSourceException, null);
+            parent::__construct('The source is invalid', ExceptionCodes::InvalidSourceException);
         }
     }
